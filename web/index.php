b70c9d3326f6f1aab2e33d950402598a8144e5e1
Charlotte W <cwelkin@icloud.com>
2014-09-26 14:24:33 +0100

    Comments

--- web/index.php
+++ web/index.php
@@ -1,42 +1,45 @@
 <?php
  
-// set_time_limit(0);
-
 require_once __DIR__.'/../vendor/autoload.php';
 
-date_default_timezone_set('UTC');
+date_default_timezone_set('UTC');   // compulsory thing to stop stupid errors
  
 $app = new Silex\Application();
  
-$app["debug"] = true;
+$app["debug"] = true;   // you'll want to turn this to false in production
  
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
 $app->get("/", function () use ($app) {
+    // declare php-github clients
     $client = new \Github\Client($client);
-
     $paginator  = new Github\ResultPager($client);
 
+    // get .env file and load the token from it
     Dotenv::load(__DIR__.'/../');
-
     $token = getenv('GITHUB_TOKEN');
 
+    // authenticate with github
     $client->authenticate($token, Github\Client::AUTH_HTTP_TOKEN);
 
+    // get the user and the user repositories
     $user = $client->api('organization')->show('purplapp');
 	$repositories = $client->api('repo')->show('purplapp', 'purplapp');
 
     // $repo_contributors = $client->api('repo')->contributors('purplapp', 'purplapp', false);
 
+    // get the languages used in the repository
     $repo_language = $client->api('repo')->languages('purplapp', 'purplapp');
     // $repo_language = new LanguageCollection($repo_language);
 
+    // get the pull requests for the repository
     $repo_pull = $client->api('pull_request')->all('purplapp', 'purplapp', array('state' => 'all'));
     $repo_pull_comments_response = $client->getHttpClient()->get('/repos/purplapp/purplapp/comments');
     $repo_pull_comments = Github\HttpClient\Message\ResponseMediator::getContent($repo_pull_comments_response);
 
+    // get the releases from the repository
     $repo_releases = $client->api('repo')->releases()->all('purplapp', 'purplapp');
 
     // get total number of commits
